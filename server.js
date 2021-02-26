64c3daa65bbc02bd902af03e91f94bfa7e9430fc
Lautaro Alessandro <saantialessa@gmail.com>
2021-02-26 09:36:35 -0300

    ready to deploy

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express') //Libreria que nos facilita montar un servidor de manera mas facil
+const path = require('path')
 require('dotenv').config() //En el config se le puede agregar por ej que el dotenv lo haga funcionar en otro archivo.
 const cors = require('cors') //Libreria que nos permite recibir llamados a mi servidor desde cualquier lado
 const router = require('./routes/index')
@@ -14,4 +15,12 @@ app.use(express.json())
 
 app.use('/api', router)
 
-app.listen(4000, ()=> console.log("app listening on port 4000"))
+if(process.env.NODE_ENV === 'production'){
+  app.use(express.static('client/build'))
+  app.get('*', (req, res)=>{
+    res.sendFile(path.join(__dirname+"/client/build/index.html"))
+  })
+}
+
+const PORT = process.env.PORT
+app.listen(PORT,  ()=> console.log('App listening on port' + PORT ))
